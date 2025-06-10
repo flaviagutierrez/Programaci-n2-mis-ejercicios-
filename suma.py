33b217ebc5b44bcc58a9127c8d537c8f48e12444
flaviagutierrez <flaviagutierrez.2201@gmail.com>
2025-06-10 13:34:03 +0000

    Suma Practica

---
+++ suma.py
@@ -0,0 +1,4 @@
+def sumar(a, b): # "a" y "b" son PARÁMETROS
+ """Esta función recibe dos números y devuelve su suma."""
+ resultado_suma = a + b
+ return resultado_suma # La palabra clave "return" devuelve un valor
