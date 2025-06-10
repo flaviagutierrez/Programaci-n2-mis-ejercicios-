ca556764866cff3d36cf08d5c0eb263ce3073f7e
flaviagutierrez <flaviagutierrez.2201@gmail.com>
2025-06-10 12:50:00 +0000

    Bucle for practica

---
+++ Bucle.py
@@ -0,0 +1,8 @@
+print("Contando hasta 3 (sin incluirlo):")
+for number in range(3):  # range(3) genera 0, 1, 2
+    print(number) 
+
+print("\nRecorriendo un string:")
+nombre = "PYTHON"
+for letra in nombre:
+    print(letra)
