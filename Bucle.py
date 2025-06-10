5ec8eeb7c3ba137eff444f97042a8d24c618884c
flaviagutierrez <flaviagutierrez.2201@gmail.com>
2025-06-10 13:20:02 +0000

    Practica Saludo

--- Bucle.py
+++
@@ -1,8 +0,0 @@
-print("Contando hasta 3 (sin incluirlo):")
-for number in range(3):  # range(3) genera 0, 1, 2
-    print(number) 
-
-print("\nRecorriendo un string:")
-nombre = "PYTHON"
-for letra in nombre:
-    print(letra)
