32b93670e3641fcb18bced54f49d3c173339705a
flaviagutierrez <flaviagutierrez.2201@gmail.com>
2025-06-11 12:18:32 +0000

    While practica

---
+++ While.py
@@ -0,0 +1,7 @@
+contador = 0
+print("\nBucle while:")
+while contador < 3:
+ print(f"Contador es: {contador}")
+ contador = contador + 1 # ¡MUY IMPORTANTE! Actualizar la variable de control
+ # para evitar un bucle infinito.
+print("¡Bucle while terminado!")
