02b535e2bc15cea17ed5ccd9c77b67cc749187b0
flaviagutierrez <flaviagutierrez.2201@gmail.com>
2025-06-17 12:35:27 +0000

    ACT

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,13 +1,25 @@
-def busqueda_binaria(lista, elemento):
-  inicio=0
-  fin=len(lista) - 1
-  while inicio<=fin:
-    medio=(inicio+fin)//2
-    valor_medio=lista[medio]
-    if valor_medio==elemento:
-      return medio
-    elif elemento>valor_medio:
-      inicio=medio+1
-    else:
-      fin=medio-1
-  return -1
+def busqueda_binaria(lista, objetivo):
+  inicio = 0
+  fin = len(lista) - 1
+
+  while inicio <= fin:
+      medio = (inicio + fin) // 2
+
+      if lista[medio] == objetivo:
+          return medio  # Retorna el índice donde se encontró el objetivo
+      elif lista[medio] < objetivo:
+          inicio = medio + 1
+      else:
+          fin = medio - 1
+
+  return -1  # Retorna -1 si el objetivo no está en la lista
+
+# Ejemplo de uso
+lista_ordenada = [1, 3, 5, 7, 9, 11, 13]
+objetivo = 7
+
+resultado = busqueda_binaria(lista_ordenada, objetivo)
+if resultado != -1:
+  print(f"Elemento encontrado en el índice {resultado}")
+else:
+  print("Elemento no encontrado")
